ddac4794ffc282ac67af9555e12dce32b4bc32f8
yuanjunjie <yuanjunjie@kedacom.com>
2018-07-06 18:09:39 +0800

    replace path in pc file

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,7 @@
 from conans import ConanFile, CMake, tools
 import os
 import platform
+import re
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,6 +30,18 @@ class NodePlugin(ConanFile):
         self.options["tesseract"].shared = True
 
     def requirements(self):
+        if self.settings.os == 'Linux':
+            if not os.getenv('PKG_CONFIG_EXECUTABLE'):
+                self.run(
+                    "wget https://pkg-config.freedesktop.org/releases/pkg-config-0.29.1.tar.gz",
+                    cwd="/tmp")
+                self.run("tar -zxf pkg-config-0.29.1.tar.gz", cwd="/tmp")
+                self.run(
+                    "./configure --prefix=/usr --with-internal-glib --disable-host-tool --docdir=/usr/share/doc/pkg-config-0.29.1",
+                    cwd="/tmp/pkg-config-0.29.1")
+                self.run("make", cwd="/tmp/pkg-config-0.29.1")
+                self.run("sudo make install", cwd="/tmp/pkg-config-0.29.1")
+
         try:
             if self.settings.os == 'Linux':
                 self.run("sudo conan remote add upload_tesseract \
@@ -44,17 +57,12 @@ class NodePlugin(ConanFile):
             self.options.remove("fPIC")
 
     def build(self):
-        if self.settings.os == 'Linux':
-            self.run(
-                "wget https://pkg-config.freedesktop.org/releases/pkg-config-0.29.1.tar.gz",
-                cwd="/tmp")
-            self.run("tar -zxf pkg-config-0.29.1.tar.gz", cwd="/tmp")
-            self.run(
-                "./configure --prefix=/usr --with-internal-glib --disable-host-tool --docdir=/usr/share/doc/pkg-config-0.29.1",
-                cwd="/tmp/pkg-config-0.29.1")
-            self.run("make", cwd="/tmp/pkg-config-0.29.1")
-            self.run("sudo make install", cwd="/tmp/pkg-config-0.29.1")
-
+        for p in self.deps_cpp_info.lib_paths:
+            pc_file = self.getallfile(p)
+            if pc_file:
+                searchObj = re.search('/(.*)/.conan/data', pc_file)
+                if searchObj:
+                    self.replace_pc(pc_file, searchObj.group(1))
         options = {
             'arch': 'x64',
             'compiler': '',
@@ -94,3 +102,38 @@ class NodePlugin(ConanFile):
     def package(self):
         self.copy(pattern="*", dst=".", src="out")
         self.copy(pattern='test.bmp', dst="test", src="plugin/test")
+
+    def package_info(self):
+        for p in self.deps_cpp_info.lib_paths:
+            pc_file = self.getallfile(p)
+            if pc_file:
+                searchObj = re.search('/(.*)/.conan/data', pc_file)
+                if searchObj:
+                    self.replace_pc(pc_file, searchObj.group(1))
+
+    def getallfile(self, path):
+        allfilelist = os.listdir(path)
+        for file in allfilelist:
+            filepath = os.path.join(path, file)
+            if os.path.isdir(filepath):
+                res = self.getallfile(filepath)
+                if res:
+                    return res
+            if ".pc" in filepath:
+                return filepath
+
+    def replace_pc(self, target_file, target_dir):
+        file_object = open(target_file, 'r+')
+        try:
+            all_lines = file_object.readlines()
+            file_object.seek(0)
+            file_object.truncate()
+            for line in all_lines:
+                if '.conan/data' in line:
+                    searchObj = re.search('/(.*)/.conan/data', line)
+                    if searchObj:
+                        line = line.replace("%s" % searchObj.group(1),
+                                            target_dir)
+                file_object.write(line)
+        finally:
+            file_object.close()
